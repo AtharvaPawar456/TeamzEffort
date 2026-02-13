46a4f6cf4a304f451a820474e4b2b50ead6d50d4
Atharva Prashant Pawar <67649944+AtharvaPawar456@users.noreply.github.com>
2026-02-13 11:16:34 +0530

    event imgs

--- static/galleryimgs/renamingCode.py
+++ static/galleryimgs/renamingCode.py
@@ -42,25 +42,29 @@ folderList = [
     # "NewYearCelebration_Jan_2026",
     # "DXNAnnualRecognitionNite8Jan2026/v2",
     # "DXN_AchiverTraining9Jan2026",
-    "Diwali_Celebration_2025",
+    # "Diwali_Celebration_2025",
+    # "DXN_PalgharDivesh_2025",
+    # "DXN_StarAgentEvent_2025",
+    # "DXN_GetTogether_2025",
     
     
 ]
-for folder in folderList:
-    renameAllFiles(folder)
+# for folder in folderList:
+#     renameAllFiles(folder)
 
 
 # files = os.listdir("DXN_AchiverTraining9Jan2026")
 # files = os.listdir("DXNAnnualRecognitionNite8Jan2026/v2")
-files = os.listdir("Diwali_Celebration_2025")
+files = os.listdir("DXN_PalgharDivesh_2025")
 # # # tempPath = "https://raw.githubusercontent.com/AtharvaPawar456/TeamzEffort/refs/heads/main/static/galleryimgs/NewYearCelebration_Jan_2026/"
 # # # tempPath = "https://raw.githubusercontent.com/AtharvaPawar456/TeamzEffort/refs/heads/main/static/galleryimgs/DXNAnnualRecognitionNite8Jan2026/"
-tempPath = "https://raw.githubusercontent.com/AtharvaPawar456/TeamzEffort/refs/heads/main/static/galleryimgs/DXNAnnualRecognitionNite8Jan2026/v2/"
+# tempPath = "https://raw.githubusercontent.com/AtharvaPawar456/TeamzEffort/refs/heads/main/static/galleryimgs/DXNAnnualRecognitionNite8Jan2026/v2/"
+tempPath = "https://raw.githubusercontent.com/AtharvaPawar456/TeamzEffort/refs/heads/main/static/galleryimgs/Diwali_Celebration_2025/"
 
 
 
-for fileName in files:
-    print(f"{tempPath}{fileName};")
+# for fileName in files:
+#     print(f"{tempPath}{fileName};")
 
 
 
